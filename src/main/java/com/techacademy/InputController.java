db8d8f1cb82a6176c58854033367ec25b8eb8a44
yone03 <starmimi29@yahoo.co.jp>
2024-04-29 19:24:43 +0900

    InputController修正

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -3,20 +3,12 @@ package com.techacademy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class InputController {
     @GetMapping("/input")
-    public String getInput() {
-        // input.htmlに画面遷移
-        return "input";
-    }
-
-    @PostMapping("/input?previous={previous}/")
-    public String getPrevious(@PathVariable String previous, Model model) {
-        // パスパラメータで受け取った値をModelに登録
+    public String getInput(@RequestParam(name = "previous", required = false) String previous, Model model) {
         model.addAttribute("previous", previous);
         // input.htmlに画面遷移
         return "input";
